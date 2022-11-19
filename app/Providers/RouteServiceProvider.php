0755c2e6da60e9600afd23aac9c095f11e857baf
Nali1919 <my@email.com>
2022-11-19 13:00:50 +0100

    apporto modifiche

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -21,7 +21,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/home';
+    public const HOME = '/Admin';
 
     /**
      * Define your route model bindings, pattern filters, etc.
@@ -32,7 +32,7 @@ class RouteServiceProvider extends ServiceProvider
     {
         //
 
-        parent::boot();
+         parent::boot();
     }
 
     /**
